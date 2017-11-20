6ec55abddd4fce7ff5da46ff0a43c1c8b62d298e
Przemo04 <asus18@o2.pl>
2017-11-20 17:32:34 +0100

    configuration sass, flexbox grid, etc

--- cvprzemo/src/App.js
+++ cvprzemo/src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
+
+				<Grid fluid>
+					<Row>
+						<Col xs={12} sm={3} md={6} lg={6} >
+							Hello, world!
+							<img src={logo} className="App-logo" alt="logo" />
+						</Col>
+						<Col xs={12} sm={3} md={2} lg={6} >
+							Hello, world2!
+							<img src={logo} className="App-logo" alt="logo" />
+						</Col>
+					</Row>
+				</Grid>
       </div>
     );
   }
