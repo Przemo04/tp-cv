858fb49af3e811d955777d34139001c421058e28
Przemo04 <asus18@o2.pl>
2017-11-21 21:02:20 +0100

    Basic sidebar finish

---
+++ cvprzemo/src/sidebar/aboutDescription/AboutDescription.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './AboutDescription.scss';
+
+class AboutDescription extends React.Component{
+	render(){
+
+		return(
+			<div className="AboutDescription">
+				<h2>O mnie</h2>
+				<p>Cieszę się, że trafiłeś na moją personalną stronę ! :) </p>
+				<p>Skoro to czytasz to prawdopodobnie chcesz czegoś dowiedzieć się o mnie. A więc, postaram się opisać moją historię zawodową jak i osobistą. </p>
+			</div>
+		);
+	}
+}
+export default AboutDescription;
--- cvprzemo/src/sidebar/index.js
+++ cvprzemo/src/sidebar/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 import HeadSidebar from './headSidebar/HeadSidebar.js';
+import PersonalDetails from './personalDetails/PersonalDetails.js';
+import AboutDescription from './aboutDescription/AboutDescription.js';
+import './Sidebar.scss';
 
 class Sidebar extends React.Component{
 	render(){
 
 		return(
-			<div>
+			<div className="Sidebar">
 				<HeadSidebar />
+				<PersonalDetails />
+				<AboutDescription />
 			</div>
 		);
 	}
